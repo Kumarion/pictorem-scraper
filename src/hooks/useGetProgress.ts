a8521e4291c4fb1b6857bf7d77a08a5f9a7aa2a9
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-03-27 17:17:21 -0400

    commented out old stuff

--- src/hooks/useGetProgress.ts
+++ src/hooks/useGetProgress.ts
@@ -1,33 +1,37 @@
-import { api } from "~/utils/api";
+// import { api } from "~/utils/api";
 
-type useGetProgressProps = {
-    jobId: string;
-    scrapePending: boolean;
-    setProgress: (progress: number) => void;
-    setMaxProgress: (maxProgress: number) => void;
-}
+// type useGetProgressProps = {
+//     jobId: string;
+//     scrapePending: boolean;
+//     setProgress: (progress: number) => void;
+//     setMaxProgress: (maxProgress: number) => void;
+// }
 
-export default function useGetProgress({jobId, scrapePending, setProgress, setMaxProgress}: useGetProgressProps) {
-  // api.scraper.getProgress.useQuery({jobId}, {
-  //   enabled: scrapePending,
-  //   refetchInterval: 500,
-  //   onSuccess: (data) => {
-  //     setProgress(data.progress);
-  //     setMaxProgress(data.maxProgress);
-  //   }
-  // });
-  const subscription = api.scraper.getProgress.useSubscription({jobId}, {
-    enabled: scrapePending,
+// export default function useGetProgress({jobId, scrapePending, setProgress, setMaxProgress}: useGetProgressProps) {
+//   // api.scraper.getProgress.useQuery({jobId}, {
+//   //   enabled: scrapePending,
+//   //   refetchInterval: 500,
+//   //   onSuccess: (data) => {
+//   //     setProgress(data.progress);
+//   //     setMaxProgress(data.maxProgress);
+//   //   }
+//   // });
+//   const subscription = api.scraper.getProgress.useSubscription({jobId}, {
+//     enabled: scrapePending,
 
-    onData: (data: { progress: number; maxProgress: number }) => {
-      console.log(data);
-      setProgress(data.progress);
-      setMaxProgress(data.maxProgress);
-    },
-    onError: (error) => {
-      console.log("Subscription error" + error.message);
-    }
-  });
+//     onData: (data: { progress: number; maxProgress: number }) => {
+//       console.log(data);
+//       setProgress(data.progress);
+//       setMaxProgress(data.maxProgress);
+//     },
+//     onError: (error) => {
+//       console.log("Subscription error" + error.message);
+//     }
+//   });
   
-  return subscription;
+//   return subscription;
+// }
+
+export default function test() {
+  console.log("Test");
 }
